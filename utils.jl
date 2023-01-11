69920b8402e2fee5c45e9ccb90b61a7d599ae355
Jacob Zelko <jacobszelko@gmail.com>
2023-01-10 22:33:09 -0500

    Made comments transparent for aesthetics :sparkles:

--- utils.jl
+++ utils.jl
@@ -21,7 +21,7 @@ function hfun_addcomments()
         data-reactions-enabled="1"
         data-emit-metadata="0"
         data-input-position="top"
-        data-theme="preferred_color_scheme"
+        data-theme="transparent_dark"
         data-lang="en"
         data-loading="lazy"
         crossorigin="anonymous"
